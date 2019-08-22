6e9b759b9f6db395b7cefdd98e1d80ba363b1d56
Anderson Laurentino <alaurentino.br@gmail.com>
2019-08-22 09:35:36 -0300

    Create a n-queens algorithm

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
-def n_queens(size=(8, 8)):
-    return []
+def test_queen(row, col, queens):
+    error = False
 
-print(n_queens(size(8, 8)))
+    for (row1, col1) in enumerate(queens):
+        # Diagonal
+        error = abs(row - row1) == abs(col - col1)
+        # Vertical | Horizontal
+        error = error or (row == row1 or col == col1)
+
+        if error:
+            break
+    
+    return not error
+
+def n_queens(size=(8, 8), queens=[], row=0):
+    rows_size, cols_size = size
+
+    if rows_size == row:
+        return queens
+    
+    max_lenght = []
+    
+    for col in range(cols_size):
+        if test_queen(row, col, queens):
+            queens.append(col)
+
+            if len(queens) > len(max_lenght):
+                max_lenght = queens
+
+            forward = n_queens(size, queens[:], row+1)
+
+            if len(forward) > len(max_lenght):
+                max_lenght = forward
+
+            queens = queens[:-1]
+    
+    return max_lenght
+                
+
+print(n_queens(size=(8,8)))
